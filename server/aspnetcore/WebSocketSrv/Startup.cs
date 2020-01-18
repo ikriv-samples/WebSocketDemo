d4ff01408b02686579b3798b8ce6bbbaf7259028
Ivan Krivyakov <ivan@ikriv.com>
2020-01-18 18:14:51 -0500

    Removed commented out code

--- server/aspnetcore/WebSocketSrv/Startup.cs
+++ server/aspnetcore/WebSocketSrv/Startup.cs
@@ -49,8 +49,6 @@ namespace WebSocketSrv
                 .UseWebSockets()
                 .UseCors()
                 .UseMvcWithDefaultRoute();
-
-            //app.Use(ProcessWebSocketConnection);
         }
     }
 }
